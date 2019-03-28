31fc8cf3cedb2526b2b56ab0a85828a419054e29
hongmon1 <39690330+hongmon1@users.noreply.github.com>
2019-03-28 16:57:08 +0900

    MainLobby 애니메이션 및 UI 수정

--- 2019_1_bridge_game/Assets/UI/Scripts/History/UIButler.cs
+++ 2019_1_bridge_game/Assets/UI/Scripts/History/UIButler.cs
@@ -1,8 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIButler : UIControl
 {
-   
+    List<string> texts = new List<string>();
+    private Text text;
+    int cnt = 0;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        //임의 스크립트
+        texts.Add("1:안녕하세요");
+        texts.Add("2:반갑습니다");
+        texts.Add("3:안녕히가세요");
+    }
+    void Update()
+    {
+        
+    }
 }
