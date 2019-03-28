8614a0542546e9a2374132f0d6d7c980c4ae1ea7
momo <mjh79017901@gmail.com>
2019-03-28 22:41:47 +0900

    악보 드랍 상황에 따른 다른 사운드 출력 (피아노 각 계이름 출력) 테스트
    미리 정해진 음계에 따른 출력
    악보 보유 갯수를 index로 해서 출력 한거라
    본진 들어갔다가 나오면 다시 0부터 나옴.
    
    저작권 문제가 있을 수도 있는데 자유롭게 쓸 수 있다면
    
    계 이름 쭉 쓰인 거 풀을 여러 개 만들어서 본진 들어갔다가 나올 때
    랜덤 한 곡 음계 칠 수 있게 하면 좋을 듯?

--- 2019_1_bridge_game/Assets/GameScenes/InGame/Scripts/SheetMusic.cs
+++ 2019_1_bridge_game/Assets/GameScenes/InGame/Scripts/SheetMusic.cs
@@ -6,11 +6,24 @@ using Photon.Pun.UtilityScripts;
 
 public class SheetMusic : PickupItem
 {
+    private static readonly int[] pianoIndex =
+    {
+        4, 5, 6, 4, 9, 6, 5, 9, 5, 4, 2, 6, 4, 3, 3, 2, 3, 4, 5, 1, 4, 5, 6, 7, 7, 6, 5, 4, 5,
+4, 5, 6, 4, 9, 6, 5, 9, 5, 4, 2, 2, 3, 4, 1, 1, 2, 3, 4, 5, 1, 4, 5, 6, 7, 7, 6, 5 ,4, 4 };
+    
+//6, 7, 8, 8, 8, 8, 8, 9, 8, 7,b ,6, 6 ,6, 6 ,6, 7, 6 ,5 ,4, 4, 4, 3, 2, 3, 3, 4 }
+//    }
+//    {
+//        4, 5, 6, 4, 8, 6, 5, 8, 5, 4, 2, 6, 4, 3, 3, 2, 3, 4, 5, 1, 4, 5, 6, 7b, 7b, 6, 5, 4, 5,
+//4, 5, 6, 4, 8, 6, 5, 8, 5, 4, 2, 2, 3, 4, 1, 1, 2, 3, 4, 5, 1, 4, 5, 6, 7b, 7b, 6, 5 ,4, 4
+//6, 7b, 8, 8, 8, 8, 8, 9, 8, 7,b ,6, 6 ,6, 6 ,6, 7, 6 ,5 ,4, 4, 4, 3, 2, 3, 3, 4 }
+//}
     protected override void OnPickedUp()
     {
         if (PickupIsMine)
         {
-            Debug.Log("I picked up something. That's a score!");
+            Debug.Log("I picked up something. That's a score!, " + pianoIndex.Length);
+            AudioManager.Instance.PlaySound(pianoIndex[(PhotonNetwork.LocalPlayer.GetNumSheetMusic() % pianoIndex.Length)], SFXType.PIANO);
             PhotonNetwork.LocalPlayer.AddNumSheetMusic(1);
         }
         else
