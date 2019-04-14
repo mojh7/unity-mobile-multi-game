c05f5d841d45ee4975dee4966013eabb3db94afc
momo <mjh79017901@gmail.com>
2019-04-14 19:46:35 +0900

    Add EffectsData
    아이템 효과 내용 데이타 추가

---
+++ 2019_1_bridge_game/Assets/Item/Scripts/EffectsData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EffectsData", menuName = "Item/EffectsData", order = 0)]
+public class EffectsData : ScriptableObject
+{
+    // 합 연산
+    public float movingSpeedIncrementRate;
+
+    // 곱 연산
+    public float itemGettingRangeIncrementRate;
+
+    // t or f
+    public bool canSlide;
+}
