3e0bd4786a6f32e25bfb54084eb922cff3510b3b
kkwoo94517 <kkwoo94517@gmail.com>
2019-06-10 23:22:39 +0900

    오브젝트 추가 및 겹치는 orderSort 추가

---
+++ 2019_1_bridge_game/Assets/Map/Scripts/PositionSorterObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionSorterObject : MonoBehaviour
+{
+    [SerializeField] private int sortingOrderBase = 1000;
+    [SerializeField] private int offset = 0;
+    [SerializeField] private bool runOnlyOnce = false;
+
+    private float timer;
+    private float timerMax = 0.1f;
+    private SpriteRenderer render;
+
+    private void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    private void LateUpdate()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0.0f)
+        {
+            timer = timerMax;
+            render.sortingOrder = (int)(sortingOrderBase - (transform.position.y * 100) - offset);
+
+            if (runOnlyOnce) Destroy(this);
+        }
+    }
+}
