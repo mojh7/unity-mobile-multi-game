c00befd96b6b7ab7b3e30342bc7c080fee0b1d94
momo <mjh79017901@gmail.com>
2019-06-14 00:41:15 +0900

    스크린 터치 효과(파티클) 및 효과음(피아노 계이름 출력) 추가
    스크린 터치시 효과 추가
     - 음표 모양의 파티클 추가
     - 효과음으로 계이름 도~시 출력(순차 재생 or 랜덤 이건 더 나은걸로 추후 변경될 예정)

---
+++ 2019_1_bridge_game/Assets/Character/Scripts/ScreenTouchEffect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// TODO : 임시로 만든 화면 터치 이펙트 클래스, 추후 수정 해야됨.
+
+public class ScreenTouchEffect : MonoBehaviour
+{
+    // 오브젝트 풀 만들어서 꺼내 쓰게끔 해야 되는데 프로토타입 용으로 임시로 바로 instantiate로 복사해서 사용
+    [SerializeField] private GameObject[] touchEffectList; 
+
+    private int pianoIndex = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 touchPos = new Vector2(wp.x, wp.y);
+
+            transform.position = touchPos;
+
+            // 생성 
+            GameObject touchEffect = Instantiate(touchEffectList[Random.Range(0, touchEffectList.Length)], transform.position, Quaternion.identity) as GameObject;
+            AudioManager.Instance.PlaySound(pianoIndex, SFXType.PIANO);
+            pianoIndex = (pianoIndex + 1) % 7;
+            //pianoIndex = Random.Range(0, 7);
+            // 파괴 
+            Destroy(touchEffect.gameObject, 1f);
+        }
+    }
+}
